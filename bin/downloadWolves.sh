f2a7966384120e458ae49d56e6eb02f5523be8db
JavierUrban <59941891+JavierUrban@users.noreply.github.com>
2020-02-26 19:55:44 -0600

    Create downloadWolves.sh

---
+++ bin/downloadWolves.sh
@@ -0,0 +1,11 @@
+##Javier Urbán
+##Proyect wolves Unit5
+##Download wolves.vcf
+
+##Download the file in .vcfformat wolves and stores within the data d$
+wget https://datadryad.org/stash/downloads/file_stream/6226
+
+##Rename the downloaded file 2662 in VCF format with the name wolves.vcf
+mv 6226 ../data/wolves
+
+
